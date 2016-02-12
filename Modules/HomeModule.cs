adbf33408357abb4e79868688551a62af31b96df
David <ddiehr@yahoo.com>
2016-02-12 09:10:01 -0800

    Add (mostly) empty views pages.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,6 +12,9 @@ namespace AddressBook
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/contact_new"] = _ => {
+        return View["contact_new.cshtml"];
+      };
 
     } //closes HomeModule function
   } // closes HomeModule class
