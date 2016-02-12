91448812cfdabe605edf7f7bee4e7aafaec436ce
David <ddiehr@yahoo.com>
2016-02-12 09:41:54 -0800

    finish contact_new and contact_created

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -15,6 +15,14 @@ namespace AddressBook
       Get["/contact_new"] = _ => {
         return View["contact_new.cshtml"];
       };
+      Get["/contact_all"] = _ => {
+        return View["contact_all.cshtml"];
+      };
+      Post["contact_created"] = _ => {
+        Contact newContact = new Contact(Request.Form["new-name"], Request.Form["new-phone"], Request.Form["new-address"]);
+        return View["contact_created.cshtml", newContact];
+      };
+
 
     } //closes HomeModule function
   } // closes HomeModule class
